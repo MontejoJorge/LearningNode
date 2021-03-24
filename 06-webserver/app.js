6e1f41024706f30c9bd510832abf7aa0b6049d45
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-03-24 11:22:08 +0100

    06-webserver

---
+++ 06-webserver/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//contenido estatico
+app.use( express.static("public") );
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + "/public/generic.html");
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + "/public/elements.html");
+})
+ 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + "/public/404.html");
+})
+
+app.listen(8080);
