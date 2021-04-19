64036edd9d49fee58d8316d5dc18c39d2e0300d0
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-04-19 15:30:10 +0200

    07-restserver

---
+++ 07-restserver/models/usuario.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const UsuarioSchema = Schema({
+    nombre : {
+        type: String,
+        required: [true, "El nombre es obligatorio"]
+    },
+    correo : {
+        type: String,
+        required: [true, "El correo es obligatorio"],
+        unique: true
+    },
+    password : {
+        type: String,
+        required: [true, "La contraseña es obligatorio"],
+    },
+    img : {
+        type: String
+    },
+    role : {
+        type: String,
+        required: [true, "El rol es obligatorio"],
+        enum: ["admin", "user",]
+    },
+    estado : {
+        type: Boolean,
+        default: true
+    },
+    google : {
+        type: Boolean,
+        default: false
+    },
+
+});
+
+module.exports = model( "Usuario", UsuarioSchema );
